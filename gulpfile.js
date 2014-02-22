8c9a0db2ca4abd0efe22937e872eb8f00d4a7330
Pawel Czekaj <pawel.czekaj@gmail.com>
2014-02-22 09:23:18 +0100

    Livereload with gulp, dependencies

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp'),
+	open = require('gulp-open'),
+	jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish'),
+
+	lr = require('tiny-lr'),
+	path = require('path'),
+	http = require('http'),
+	server = lr(),
+	connect = require('connect'),
+	livereload = require('gulp-livereload');
+
+
+gulp.task('jshint', function() {
+	gulp.src('./js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish))
+		.pipe(livereload(server));
+});
+
+
+gulp.task('open', function() {
+	gulp.src('./index.html').pipe(open('', {
+		url: "http://localhost:9000",
+	}));
+});
+
+gulp.task('server', function() {
+
+	var app = connect()
+				.use(connect.static(path.resolve('.')))
+				.use(connect.directory(path.resolve('.')));
+
+	http.createServer(app).listen(9000);
+
+	server.listen(35729, function(err) {
+		if(err) return console.log(err);
+	});
+
+	gulp.watch('./*.html', function() {
+		gulp.src('./*.html')
+			.pipe(livereload(server));
+	});
+
+	gulp.watch('./js/*.js', function() {
+		gulp.run('jshint');
+	});	gulp.watch('./sass/*.scss', function() {
+		gulp.run('sass');
+	});
+
+	gulp.run('jshint');
+});
